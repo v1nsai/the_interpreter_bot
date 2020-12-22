1c7b058bbe13969036c3fd42ba8165f9cbaea382
doctor_ew <doctor_ew@protonmail.com>
2020-12-22 02:55:11 +0000

    updated for changes in API

--- main.py
+++ main.py
@@ -10,17 +10,17 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 target_language = ''
 
 # Make the bot talk to just me and my friends, sorry I'd share if Google were free!
-allowed_ids = open('allowed_ids.txt').read().splitlines()
+allowed_ids = open('allowed_ids').read().splitlines()
 allowed_ids = [int(i) for i in allowed_ids]
 
 # Set API tokens, I had issues with setting the var in ~/.profile
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="google_key.json"
-bot_token = open('bot_token.txt', 'r').read().rstrip('\n')
+bot_token = open('bot_token', 'r').read().rstrip('\n')
 
 # Instantiates clients for Google Translate and Telegram
 client = translate.Client()
 bot = telegram.Bot(token=bot_token)
-updater = Updater(token=bot_token)
+updater = Updater(token=bot_token, use_context=True)
 dispatcher = updater.dispatcher
 
 # Reads all messages sent to it, determines the language and translates to or from target_language
@@ -41,25 +41,22 @@ def auto_translate(update, context):
     context.bot.send_message(chat_id=update.message.chat_id, text=html.unescape(trans['translatedText']))
 
 # Set the language to translate to and from
-def set_target_language(bot, update, args):
+def set_target_language(update, context):
     if update.message.chat_id not in allowed_ids:
         bot.send_message(chat_id=update.message.chat_id,
                          text="Sorry I'm not allowed to talk to strangers.  Send a message to @doctor_ew to learn more about me")
         return
     global target_language
-    if len(args) < 1 or len(args) > 1:
+    if len(context.args) < 1 or len(context.args) > 1:
         bot.send_message(chat_id=update.message.chat_id,
                          text='Set language with /set_target_language <2 letter language code>')
         return
-    target_language = args[0]
+    target_language = context.args[0]
     bot.send_message(chat_id=update.message.chat_id, text='Target language set to ' + target_language)
 
 # Create handlers and add them to the dispatcher
-auto_translate_handler = MessageHandler(Filters.text, auto_translate)
-set_target_language_handler = CommandHandler('set_target_language', set_target_language, pass_args=True)
-
-dispatcher.add_handler(auto_translate_handler)
-dispatcher.add_handler(set_target_language_handler)
+dispatcher.add_handler(CommandHandler("set_target_language", set_target_language))
+dispatcher.add_handler(MessageHandler(Filters.text, auto_translate))
 
 # Starts listening for messages until the script is stopped
 updater.start_polling()
